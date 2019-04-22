52562afa443824d6e3424947d148dfc3ca96aa74
Dat Truong <dat.truonghuu1212@gmail.com>
2019-04-22 21:23:51 +0300

    First commit

---
+++ Study_Manager/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Study_Manager
+//
+//  Created by Dat Truong on 22/04/2019.
+//  Copyright © 2019 Dat Truong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
